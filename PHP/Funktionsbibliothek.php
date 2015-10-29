6d5b1a012912561f938971d55d4a844c67866b07
MadMirk <MadMirk@users.noreply.github.com>
2015-10-29 12:45:40 +0100

    Update Funktionsbibliothek.php

--- PHP/Funktionsbibliothek.php
+++ PHP/Funktionsbibliothek.php
@@ -4,8 +4,21 @@
 */
 //------Variablen------
 <?php
-$text = 'Hello World.';
+$string = 'Hello World.'; OR $_ = 'Hello World.'; OR $s = 'Hello World.';
+$int = 42;
+$float = 42.;
+$bool = true;
 ?>
 
 //-----einbinden von Variablen in HTML------
-<input type="text" value="<?php echo $text; ?>">
+<input type="text" value="<?php echo $string; ?>">
+
+/*-----Activate Error Reporting-----
+php.ini --> Error handling and logging
+Change --> error_reporting = E_ALL & E_STRICT
+
+OR
+
+in PHP
+<?php error_reporting(E_ALL); OR ini_set('error_reporting','E_ALL'); ?>
+*/
