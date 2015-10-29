7e2031cbeba8ca7524c2c72fa0e8a7b8e17c028a
MadMirk <MadMirk@users.noreply.github.com>
2015-10-29 12:16:11 +0100

    Create Funktionsbibliothek.php
    Sammlung an grundsätzlichen Funktionen und Mechanismen

---
+++ PHP/Funktionsbibliothek.php
@@ -0,0 +1,9 @@
+#Sammlung einfacher Funktionen und Mechanismen in PHP
+
+------Variablen------
+<?php
+$text = 'Hello World.';
+?>
+
+-----einbinden von Variablen in HTML------
+<input type="text" value="<?php echo $text; ?>">
