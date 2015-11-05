980d4c16dc9050d836cdfe420358383386455b79
MadMirk <MadMirk@users.noreply.github.com>
2015-11-05 01:17:20 +0100

    Update datensatzAnlegen.php

--- PHP/datensatzAnlegen.php
+++ PHP/datensatzAnlegen.php
@@ -1,32 +1,16 @@
 <?php
-//FUNKTION --- Uhrzeit deadlineDays errechnen
-date_default_timezone_set("Europe/Berlin");
-$timestamp = time();
 
-
-$dateCurrent = new DateTime((int)$_POST['Jahr']."-".(int)$_POST['Monat']."-".(int)$_POST['Tag']);
-
-echo "-------------------";
-$dateEvent = new DateTime(date("Y-m-d",$timestamp));
-$interval = $date1->diff($date2);
-echo "difference " . $interval->y . " years, " . $interval->m." months, ".$interval->d." days "; 
-
-// shows the total amount of days (not divided into years, months and days like above)
-echo "difference " . $interval->days . " days ";
 //benötigte Variablen
 $id;
 
 $name = $_POST['Titel'];
 $description = $_POST['Beschreibung'];
 $deadline = $_POST['Deadline'];
-$deadlineDays;
+$leftDays = strtotime((int)$_POST['Jahr']."-".(int)$_POST['Monat']."-".(int)$_POST['Tag'])
 $ownPriority = $_POST['Priorität'];
 $fixDate = $_POST['FesterTermin'];
 
 $finalPriority;
-
-//echo $name; 
-//echo $description;
 
 ?>
 
