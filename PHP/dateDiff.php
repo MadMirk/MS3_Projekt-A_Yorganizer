94555ff24dbbb9437faa3be4ffcfe060ff908052
MadMirk <MadMirk@users.noreply.github.com>
2015-11-25 18:24:24 +0100

    Update dateDiff.php

--- PHP/dateDiff.php
+++ PHP/dateDiff.php
@@ -1,11 +1,10 @@
 <?php
 //FUNKTION --- Differenz zweier Daten in Tagen
-	SESSION_START(); 
 	
 	$now = time();
-    $_SESSION["deadline"] = strtotime((int)$_POST['Jahr']."-".(int)$_POST['Monat']."-".(int)$_POST['Tag']);
-    $_SESSION["leftDays"] = $now - $_SESSION["deadline"];
-	echo floor($_SESSION["leftDays"]/(60*60*24));
+    $deadline = strtotime($event[3]); //strtotime((int)$_POST['Jahr']."-".(int)$_POST['Monat']."-".(int)$_POST['Tag']);
+    $leftDays = $now - $deadline;
+	echo abs(floor($leftDays/(60*60*24)));
     	//Quelle DateDiff: http://stackoverflow.com/questions/2040560/finding-the-number-of-days-between-two-dates
     	//Quelle Variablen in mehreren voneinander unabhängigen PHP Scripten aurfurbar machen:
     	//http://www.php-kurs.info/tutorial-variablen_uebergeben_include.html
