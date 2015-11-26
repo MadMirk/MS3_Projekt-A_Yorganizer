345f924176bd4cc08ded7e30cf9995ac72ef44e5
MadMirk <MadMirk@users.noreply.github.com>
2015-11-26 22:23:23 +0100

    Create showEvents.php

---
+++ PHP/showEvents.php
@@ -0,0 +1,15 @@
+<?php
+	include 'readEvent.php';
+	
+	for($i = 0; $i < count($tempAllEvents); $i++)
+	{
+	echo "Titel: <b>".$tempAllEvents[$i][1]."</b><br>
+	Beschreibung: ".$tempAllEvents[$i][2]."<br>
+	Datum: ".$tempAllEvents[$i][3]".<br>
+	eigene Priorität: ".$tempAllEvents[$i][4]."<br>
+	Fester Termin: ".$tempAllEvents[$i][5]".<br>
+	<br>
+	Noch ";
+	include 'dateDiff.php'; 	
+	}	
+?>
