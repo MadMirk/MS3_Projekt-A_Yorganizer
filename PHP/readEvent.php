5851f193a01ac5292a1f4aa726323ed33a2f9a6a
MadMirk <MadMirk@users.noreply.github.com>
2015-11-26 22:22:49 +0100

    Update readEvent.php

--- PHP/readEvent.php
+++ PHP/readEvent.php
@@ -1,5 +1,8 @@
 <?php
 $datei = file("events.txt");
+$tempAllEvents[]=[];
+$a=0;
+
 foreach($datei AS $ausgabeDAT)
 {
 	$zerlegung = explode("==", $ausgabeDAT);
@@ -7,17 +10,23 @@ foreach($datei AS $ausgabeDAT)
 	foreach($zerlegung AS $ausgabeEVE)
 	{
 		$event = explode("|", $ausgabeEVE);
-		$tempID = $event[0]+1;
 		
-		echo "Titel: <b>$event[1]</b><br>
-		Beschreibung: $event[2]<br>
-		Datum: $event[3]<br>
-		eigene Priorität: $event[4]<br>
-		Fester Termin: $event[5]<br>
-		<br>
-		Noch ";
-		include 'dateDiff.php'; 		
-   }
-   echo "<br><br>";
+		$tempAllEvents[$a][0] = $event[0];
+		$tempAllEvents[$a][1] = $event[1];
+		$tempAllEvents[$a][2] = $event[2];
+		$tempAllEvents[$a][3] = $event[3];
+		$tempAllEvents[$a][4] = $event[4];
+		$tempAllEvents[$a][5] = $event[5];
+		
+		//wird in createEvent.php benötigt, um die Identifikationsnummer der einzelnen Events hochzuzählen	
+		$tempID = $event[0]+1;			
+   }  
+   echo "vorA".$a;
+   $a+=1;
+   echo "nachA".$a;
 }
+echo "COUNT".count($tempAllEvents);
+echo "<br><br>".$tempAllEvents[0][1]."<br>";
+echo "<br><br>".$tempAllEvents[1][1]."<br>";
+echo "<br><br>".$tempAllEvents[2][1]."<br>";
 ?>
