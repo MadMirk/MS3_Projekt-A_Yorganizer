da9c64bbc2bba6b259374ca8aaf475819f21e4c5
MadMirk <MadMirk@users.noreply.github.com>
2015-11-27 17:12:37 +0100

    Update loadEvents.php

--- PHP/loadEvents.php
+++ PHP/loadEvents.php
@@ -10,23 +10,11 @@ foreach($datei AS $ausgabeDAT)
 	foreach($zerlegung AS $ausgabeEVE)
 	{
 		$event = explode("|", $ausgabeEVE);
-		
-		$tempAllEvents[$a][0] = $event[0];
-		$tempAllEvents[$a][1] = $event[1];
-		$tempAllEvents[$a][2] = $event[2];
-		$tempAllEvents[$a][3] = $event[3];
-		$tempAllEvents[$a][4] = $event[4];
-		$tempAllEvents[$a][5] = $event[5];
+		$tempAllEvents[$a] = array($event[0],$event[1],$event[2],$event[3],$event[4],$event[5]);
 		
 		//wird in createEvent.php benötigt, um die Identifikationsnummer der einzelnen Events hochzuzählen	
-		$tempID = $event[0]+1;			
-   }  
-   echo "vorA".$a;
-   $a+=1;
-   echo "nachA".$a;
+		$tempID = $event[0]+1;		
+		$a+=1;	
+   }    
 }
-echo "COUNT".count($tempAllEvents);
-echo "<br><br>".$tempAllEvents[0][1]."<br>";
-echo "<br><br>".$tempAllEvents[1][1]."<br>";
-echo "<br><br>".$tempAllEvents[2][1]."<br>";
 ?>
