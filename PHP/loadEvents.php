0b0ede4fb3f17f485e3af7396f548bc8cfa6e2e9
MadMirk <MadMirk@users.noreply.github.com>
2015-12-05 14:50:05 +0100

    Update loadEvents.php
    - Das Array wurde um ein Feld in der zweiten Dimension erweitert, um die Priorität zu errechnen
    - die Anzahl der Array-Elemente in der ersten Dimension, werden in eine extra Variable gespeichert --> Performance Vorteil bei Schleifen
    - es wird die CalculatePriority eingebunden, welche die endgütlige Priorität der Events prüft.

--- PHP/loadEvents.php
+++ PHP/loadEvents.php
@@ -2,7 +2,6 @@
 $datei = file("events.txt");
 $tempAllEvents[]=[];
 $a=0;
-
 foreach($datei AS $ausgabeDAT)
 {
 	$zerlegung = explode("==", $ausgabeDAT);
@@ -10,11 +9,13 @@ foreach($datei AS $ausgabeDAT)
 	foreach($zerlegung AS $ausgabeEVE)
 	{
 		$event = explode("|", $ausgabeEVE);
-		$tempAllEvents[$a] = array($event[0],$event[1],$event[2],$event[3],$event[4],$event[5],$event[6]);
+		$tempAllEvents[$a] = array($event[0],$event[1],$event[2],$event[3],$event[4],$event[5],$event[6],"");
 		
 		//wird in createEvent.php benötigt, um die Identifikationsnummer der einzelnen Events hochzuzählen	
 		$tempID = $event[0]+1;		
 		$a+=1;	
    }    
 }
+$CountEvents = count($tempAllEvents);
+include 'calculatePriority.php';
 ?>
