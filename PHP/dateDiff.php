cdf31bde33d2ffd95fa97e190340fb49f6e4f562
MadMirk <MadMirk@users.noreply.github.com>
2016-01-15 00:17:05 +0100

    Update dateDiff.php

--- PHP/dateDiff.php
+++ PHP/dateDiff.php
@@ -1,27 +1,34 @@
 <?php
-//FUNKTION --- Differenz zweier Daten in Tagen/Monaten/Jahren
+//FUNKTION --- Differenz zweier Daten in Tagen/Mren
 	
+	//aktuelle Zeit
 	$now = time();
+	
+	//Fristtermin
     $deadline = strtotime($tempAllEvents[$i][3]);
+    
+    //Differenz zwischen aktueller Zeit und Fristtermin
     $leftDays = $now - $deadline;
+    
     
     if(abs(floor($leftDays/(60*60*24))) >= 365)
     {		
-		//echo round(abs(floor($leftDays/(60*60*24)))/365,2)." Jahre!.";		
+		//echo round(abs(floor($leftDays/(60*60*24)))/365,2)." Jahre!.";
+		//runterrechnen auf Jahre		
 		$leftDays = abs(floor($leftDays/(60*60*24)));
 	
 	}
     elseif(abs(floor($leftDays/(60*60*24))) >= 30)
     {
 		//echo round(abs(floor($leftDays/(60*60*24)))/30,2)." Monate!.";
-		
+		//runterrechnen auf Monate	
 		$leftDays=abs(floor($leftDays/(60*60*24)));
 	}
 	
 	else
 	{
 		//echo abs(floor($leftDays/(60*60*24)))." Tage!.";
-		
+		//runterrechnen auf Tage	
 		$leftDays=abs(floor($leftDays/(60*60*24)));
 	}
     	//Quelle DateDiff: http://stackoverflow.com/questions/2040560/finding-the-number-of-days-between-two-dates
