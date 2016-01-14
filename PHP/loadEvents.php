c5d3de87404672594d9f941e1fa1776d1c4b72d3
MadMirk <MadMirk@users.noreply.github.com>
2016-01-15 00:18:24 +0100

    Update loadEvents.php

--- PHP/loadEvents.php
+++ PHP/loadEvents.php
@@ -1,7 +1,15 @@
 <?php
+//Der Inhalt der Datei events.txt wird in eine Variable gespeichert
 $datei = file("events.txt");
+
+//Erzeugung des Arrays, wo am Ende alle Events gespeichert werden. 
+//Da jedes Event mehrere Felder benötigt und es mehrere Events gibt, verwende ich ein 2-Dimensionales Array
 $tempAllEvents[]=[];
+
+//Inkrementierungsvariable zum hochzählen innerhlab der zweiten Schleife
 $a=0;
+
+//Mit zwei Schleifen und den "explode" Befehl, trenne ich die Zeichenkette anhand der definierten Trennzeichen
 foreach($datei AS $ausgabeDAT)
 {
 	$zerlegung = explode("==", $ausgabeDAT);
@@ -9,6 +17,8 @@ foreach($datei AS $ausgabeDAT)
 	foreach($zerlegung AS $ausgabeEVE)
 	{
 		$event = explode("|", $ausgabeEVE);
+		
+		//Nach dem Trennen werden die einzelnen Werte für das jeweilige Event, in das mehrdimensionale Array geschrieben
 		$tempAllEvents[$a] = array($event[0],$event[1],$event[2],$event[3],$event[4],$event[5],$event[6],"");
 		
 		//wird in createEvent.php benötigt, um die Identifikationsnummer der einzelnen Events hochzuzählen	
@@ -16,6 +26,11 @@ foreach($datei AS $ausgabeDAT)
 		$a+=1;	
    }    
 }
+//es wird die Gesamtzahl der vorhandenen Events in eine Variable gespeichert, da diese an verschiedensten Stellen gebraucht wird
+//allerdings kostet das zählen Zeit und Leistung, weshalb sich das speichern in eine Variabel lohnt.
+//Gerade wenn man den Wert inSchleifen benutzt, würde bei jedem Schleifendurchlauf gezählt werden müssen.
 $CountEvents = count($tempAllEvents);
+
+//Aufrufen eines Scriptes, um die Priorität der einzelnen Events zu berechnen
 include 'calculatePriority.php';
 ?>
